f00002ef0e6b71f764602106815bf5eb7015b2ad
LuBly <lumenize@ajou.ac.kr>
2022-02-10 09:57:06 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -21,5 +21,15 @@ function login(){
         body: JSON.stringify(req)//오브젝트를 json 형태로 바꿔주는 명령어
     })
     .then((res)=>res.json())
-    .then(console.log);
+    .then((res)=>{
+        if(res.success){
+            location.href ="/";
+        }
+        else{
+            alert(res.msg);
+        }
+    })
+    .catch((err)=>{
+        console.error(new Error("로그인 중 에러 발생"));
+    });
 }
